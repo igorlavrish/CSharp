24a559322be6a48028d43fadf91722d6361870b1
IGOR <gosha.lavrishev@yandex.ru>
2023-09-19 19:42:46 +0300

    Task00 is over

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -9,3 +9,10 @@
 // -3 -> 9
 // -7 -> 49
 
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+
+//Console.WriteLine(number + " -> " + square);
+
+Console.WriteLine($"{number} -> {square}");
