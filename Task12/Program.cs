5e1c86b741d53feb33793a420fd80beae4773085
IGOR <gosha.lavrishev@yandex.ru>
2023-09-26 18:10:33 +0300

    Task16 done

---
+++ Task12/Program.cs
@@ -0,0 +1,24 @@
+﻿// 12. Напишите программу, которая 
+// 1/ будет принимать на вход два числа 
+// 2/ и выводить, является ли первое число кратным второму. 
+// 3/ Если первое число не кратно второму числу, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+int Remainder(int firstPar, int secondPar);
+{
+    return firstPar % secondPar;
+}
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int remainder = Remainder(number1, number2);
+
+if (remainder == 0)
+Console.WriteLine("Кратно");
+else 
+Console.WriteLine($"Не кратно, остаток {remainder}");
---
+++ Task16/Program.cs
@@ -0,0 +1,22 @@
+﻿// 16. Напишите программу, которая 
+// 1/ принимает на вход два числа 
+// 2/ и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+bool Square(int num1, int num2)
+{
+    return num2 == num1 * num1 || num1 == num2 * num2;
+    
+}
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = Square(number1, number2);
+Console.WriteLine(result ? "да" : "нет");
